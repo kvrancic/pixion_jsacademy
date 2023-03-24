094fc5506e92cdfeeba26d85084b575d74bdcb3b
kvrancic <kvrancic11@gmail.com>
2023-03-24 19:39:47 +0100

    week3 homework added

--- 03_react/src/App.jsx
+++ 03_react/src/App.jsx
@@ -1,45 +1,45 @@
-import styles from './App.module.css'
-import {useState} from 'react';
-
+import { useState } from 'react';
 import Counter from './components/Counter';
-
+import styles from './App.module.css';
 
 function App() {
-  //const [isCounterVisible, setCounterVisible] = useState(true);  
-  const [totalCounter, setTotalCounter] = useState(0); 
-
-  const increaseTotal = (message) => {
-    console.log('message'); 
-    setTotalCounter(totalCounter + 1);
-  }
-
-  //const counterArray = (new Array(5)).fill(1); 
-
-
+  const [total, setTotal] = useState(0);
   const [counters, setCounters] = useState([]);
+
+  const addCounter = () => {
+    const id = counters.length;
+    setCounters((prevCounters) => [...prevCounters, { id, count: 0 }]);
+  };
+
+  const deleteCounter = (id) => {
+    const index = counters.findIndex((counter) => counter.id === id);
+    if (index !== -1) {
+      setCounters((counters) => [
+        ...counters.slice(0, index),
+        ...counters.slice(index + 1),
+      ]);
+    }
+  };
+  
+
+  const increaseTotal = () => {
+    setTotal((prevTotal) => prevTotal + 1);
+  };
+
   return (
     <div className={styles.App}>
-      <header className = {styles.AppHeader}>
-        {
-          counters.map((item, index) => {
-            return(
-            <Counter initialCounter = {0}
+      <header className={styles.AppHeader}>
+        <button onClick={addCounter}>Add counter</button>
+        {counters.map(({ id, count }) => (
+          <Counter
+            key={id}
+            id={id}
+            initialCount={count}
             increaseTotal={increaseTotal}
-            total={totalCounter}
-            key = {item.id} //koristenje indexa ovdje je losa stvar, trebali bi imat neku konkretnu vrijednost
-            /> )
-          })
-        }
-       {/*  UNMOUNTANJE 
-        {isCounterVisible ? <Counter
-          initialCounter = {initialCounterApp}
-          total={totalCounter}
-          increaseTotal = {increaseTotal}
-        /> : null}  
-        <button onClick={() => setCounterVisible(!isCounterVisible) }> hide </button>
-        <Counter initialCounter = {0} increaseTotal={increaseTotal} total={totalCounter}> </Counter>
-        Ovo je total: {totalCounter} */}
-        <button onClick={() => { setCounters([...counters, {id: counters.length}])}}> add counter </button>
+            deleteCounter={deleteCounter}
+            total={total}
+          />
+        ))}
       </header>
     </div>
   );
--- 03_react/src/components/Counter.jsx
+++ 03_react/src/components/Counter.jsx
@@ -1,64 +1,42 @@
-//import {Component} from 'react';
+import { memo, useEffect, useState, useMemo } from 'react';
 
-// CLASS COMPONENT
-/* class Counter extends Component {
-    render() {
-        return (
-            <div>ovo je counter</div>
-        )
-    }
-} */
+const Counter = ({ id, initialCount, increaseTotal, deleteCounter, total }) => {
+  console.log('Counter Rendered: ', id);
 
-// FUNCTION COMPONENT
-/* const Counter = () => {
-    let counter = 0; 
-    return (
-        <div onClick={() => {counter = counter + 1;}}> ovo je counter: {counter} </div>
-    )
-} */
-// Ovo nece raditi jer nije hookano na state i onda ne osluskuje real time, moramo koristiti stateove
+  const [count, setCount] = useState(initialCount);
 
-import {memo, useEffect, useState, useMemo} from 'react';
-const Counter = (props, children) => {
-    console.log('props', props);
-    const {initialCounter, increaseTotal, total} = props; // dekonstrukcija 
-    const [counter, SetCounter] = useState({count: initialCounter});
+  useEffect(() => {
+    document.title = `Clicked ${count}`;
+  }, [count]);
 
-    useEffect(() => {
-        document.title = `Clicked ${counter.count}`;
-    }, [counter])
-    // sto ako izuzmem drugi parametar? -> onda se na nista ne subscribea i samo pri pozivu funkcije se jednom odradi ovo unutar useEffect i tjt, kao da nema useEffecta
+  useEffect(() => {
+    return () => {
+      console.log('Unmounted: ', id);
+    };
+  }, [id]);
 
-    useEffect(() => {
-        return () => {console.log( 'unmountam se' );}
-    }, [])
+  const isLessThan10 = useMemo(() => {
+    console.log('recalculating 10');
+    return count < 10;
+  }, [count]);
 
-    const isLessThan10 = useMemo(() => {
-        console.log('recalculating 10');
-        return counter.count < 10}, [counter]);
-
-/*     const save = () => {}; // POST /localhost/robot/new 
-
-    const createRobot = useCallback(() => {
-        save(counter);
-    }, [counter])
- */
-
-
-    return (
-        <div>
-            <div> ovo je counter: {counter.count} / {total} </div>
-            <div>Less than 10: {isLessThan10.toString()}</div>
-            <button onClick={() => {
-                    console.log('click');
-                    counter.count = counter.count + 1;
-                    SetCounter({...counter});
-                    increaseTotal('change total'); 
-                }}>
-                    Click me
-             </button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>
+        Counter: {count}/{total}
+      </div>
+      <div>Less than 10: {isLessThan10.toString()}</div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          increaseTotal();
+        }}
+      >
+        Click me
+      </button>
+      <button onClick={() => deleteCounter(id)}>Delete me</button>
+    </div>
+  );
+};
 
 export default memo(Counter);
